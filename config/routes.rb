64dd3ed27331dc28e01f3e2a8949e5f62b5817f9
Trevor Thomas <thomastrevor1@gmail.com>
2020-07-22 16:27:36 +0000

    Added about us page

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
 
   resources :products, only: [:index, :show]
   resources :categories, only: [:show]
+  
+  resource :abouts
+
+  get 'about' => 'abouts#show'
 
   resource :cart, only: [:show] do
     post   :add_item
