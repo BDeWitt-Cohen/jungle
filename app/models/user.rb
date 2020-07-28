7517dc5aa0dc8f074171676991ed4b1ee2fc6d18
Trevor Thomas <thomastrevor1@gmail.com>
2020-07-28 22:31:16 +0000

    Added one more user spec test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ActiveRecord::Base
   validates :email, :uniqueness => { :case_sensitive => false, message: "A user with that email already exists" }
-  validates :password, presence: true
+  validates :password, presence: true, length: { in: 6..20 }
   validates :password, confirmation: { case_sensitive: true }
-  validates :first_name presence: true 
-  validates :last_name presence: true
+  validates :first_name, presence: true 
+  validates :last_name, presence: true
 
   has_secure_password
   
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,17 +1,37 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
 
     before :each do
-      @user = User.new(:first_name 'Trevor', :last_name 'Thomas', :email 'thomastrevor1@gmail.com', :password 'stuff', :password_confirmation 'stuff')
-
+      @user = User.new(first_name: 'Trevor', last_name: 'Thomas', email: 'thomastrevor1@gmail.com', password: 'stuffed', password_confirmation: 'stuffed')
     end
 
     it "creates a new user when they have a unique email and enter matching passwords" do
       @user.valid?
       puts @user.errors.full_messages
-      expect(@product).to_not be_valid
+      expect(@user).to be_valid
     end
+    it "doesn't work with non-matching passwords" do
+      @user.password = 'things'
+      @user.valid?
+      puts @user.errors.full_messages
+      expect(@user).to_not be_valid
+    end
+    it "won't work with too short of passwords" do
+      @user.password = 'hi'
+      @user.password_confirmation = 'hi'
+      @user.valid?
+      puts @user.errors.full_messages
+      expect(@user).to_not be_valid
+    end
+  end
+  describe '.authenticate_with_credentials' do
+    before :each do
+      @user = User.new(first_name: 'Trevor', last_name: 'Thomas', email: 'thomastrevor1@gmail.com', password: 'stuffed', password_confirmation: 'stuffed')
+    end
+
+    # examples for this class method here
   end
 end
